a3298981db3728fe154df342e90d616e17e82c79
bwcheung <bwcheung@ualberta.ca>
2016-01-15 10:40:45 -0700

    screenshot added

--- server.py
+++ server.py
@@ -26,27 +26,46 @@ import SocketServer
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
-
+from os import curdir
 class MyWebServer(SocketServer.BaseRequestHandler):
     
+
     def handle(self):
+
+	self.header = "HTTP/1.1 200 OK\r\n"
+        self.type = ""
+        self.lengthHeader = "Content-Length: "
+	self.file = ""
         self.data = self.request.recv(1024).strip()
-	response = ("www/" + "index.html")
+
         print ("Got a request of: %s\n" % self.data)
 	if (self.data[4] == "/"):
-		i = 5
+		i = 4
 		namelist = list()
 		while (self.data[i] != " "):
 			namelist.append(self.data[i])
 			i = i + 1
 		namestr = ''.join(namelist)
-		if (namestr == "base.css"):
-			file = open('base.css', 'w')
-			
-	
-        self.request.sendall("OK")
 
-    
+	try:
+		if (namestr.endswith(".css")):
+			self.file = open(curdir + "/www" + namestr, "r").read()
+			self.type = "Content-Type: text/css\r\n"
+		elif (namestr.endswith(".html")):
+			self.file = open(curdir + "/www" + namestr, "r").read()
+			self.type = "Content-Type: text/html\r\n"
+		elif (namestr.endswith("/")):
+			self.file = open(curdir + "/www" + namestr + "/index.html", "r").read()
+			self.type = "Content-Type: text/html\r\n"
+		else:
+			self.request.sendall("HTTP/1.1 301 Moved Permanently\r\nLocation: " + namestr + "/\r\n")
+			print("HTTP/1.1 301 Moved Permanently\r\nLocation: " + namestr + "/\r\n")
+	except:
+		self.request.sendall("HTTP/1.1 404 Not Found\r\n")	
+
+	self.length = self.lengthHeader + str(len(self.file)) + "\r\n"		
+        self.request.sendall(self.header + self.type + self.length + "\r\n" + self.file)
+	namelist = list()
 
     
 
